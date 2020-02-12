2302c83bafd26a5eaf14550f8a5ca6f6ab633728
Timur Mishiev <timurmishiev@gmail.com>
2020-02-12 01:19:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,19 @@
 class Song
  attr_accessor :name, :artist, :genre
   @@count = 0
-  @@gene = []
+  @@genes = []
   @@artists = []
   
   def initialized(song_name, artist, genre)
-    @name = 
+    @name = song_name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist 
+  end 
+  
+  def self.count
+    @@count
+  end 
+    
